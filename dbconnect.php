815f15810fa7e95bf1b1572065ac8bca148a6eba
sarahbock <mail.sarahbock@gmail.com>
2022-01-18 13:18:03 +1000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 //Connect to database
 //$conn = new mysqli("localhost","my_user","my_password","my_db"); //change to your database details
-$conn = new mysqli('localhost', 'lovelear_opal', '.27.pIFe^fI#', 'lovelear_opal');
+$conn = new mysqli('localhost', '####', '####', '####');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
