2ded4a015f7c70e4f97fc8f954118b619f80fba3
Sarah Bock <mail.sarahbock@gmail.com>
2025-05-28 08:45:01 +1000

    bug fix

--- log.php
+++ log.php
@@ -1,25 +1,37 @@
 <?php
-header("Access-Control-Allow-Origin: *"); //allow app to access cross domain file
+header("Access-Control-Allow-Origin: *");
 
 require_once("./dbconnect.php");
 
-//$.get("log.php?table="+language+"&token="+token+"&entry="+selectedAudio+"&interaction=1", function() { });
+$entry = isset($_GET["entry"]) ? intval($_GET["entry"]) : 0;
+$interaction = isset($_GET["interaction"]) ? intval($_GET["interaction"]) : 0;
+$token = isset($_GET["token"]) ? $_GET["token"] : "";
+$table = isset($_GET["table"]) ? $_GET["table"] : "";
 
-$entry="";  if (isset($_GET["entry"])) {$entry= $_GET["entry"];}
-$interaction=""; if (isset($_GET["interaction"])) {$interaction= $_GET["interaction"];}
-$token="";  if (isset($_GET["token"])) {$token= $_GET["token"]; }
-$table="";  if (isset($_GET["table"])) {$table= $_GET["table"]; }
+// Validate table name to prevent SQL injection
+$allowed_tables = [
+    'umpila', 
+    'mangarrayi', 
+    'dharug'
+]; 
 
-if($entry!=="" && $token!=="sarah@bock.com.au"){
-    //$day=date("d"); $month=date("m"); $year=date("Y");
-	$entry = intval($entry); $interaction = intval($interaction);
-
-    $insertSQL2 ="INSERT INTO ".$table."_log (userid, entryid, interactionid, date) SELECT id , ".$entry." AS entryid, ".$interaction." AS interactionid, ".CURRENT_TIMESTAMP." AS date FROM ".$table."_tokens WHERE token='".$token."'";
-    echo $insertSQL2;
-    //$insertSQL2 = "INSERT INTO log (entryid, day, month, year) VALUES (".$entry.", ".$day.", ".$month.", ".$year.")";
-    //$insertSQL2 = "INSERT INTO log (entryid, date) VALUES (".$entry.", ".CURRENT_TIMESTAMP.")";
-    $conn->query($insertSQL2);
+if (!in_array($table, $allowed_tables)) {
+    die("Invalid table name.");
 }
 
+if ($entry && $token !== "sarah@bock.com.au") {
+    if ($token === '') {
+        $insertSQL2 = "INSERT INTO `{$table}_log` (`userid`, `entryid`, `interactionid`, `date`) VALUES ('0', '12', '4', CURRENT_TIMESTAMP);";
+    } else {
+        $token = $conn->real_escape_string($token);
+        $insertSQL2 = "INSERT INTO `{$table}_log` (userid, entryid, interactionid, date) SELECT id, {$entry}, {$interaction}, CURRENT_TIMESTAMP FROM {$table}_tokens WHERE token='{$token}'";
+    }
 
+    if (!$conn->query($insertSQL2)) {
+        echo "SQL Error: " . $conn->error;
+    } else {
+        echo "Success.";
+    }
+}
 ?>
+
